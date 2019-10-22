626d4c7f8611b1da6e80fffed41a24d30932ef54
sana <sanazez1234@gmail.com>
2019-10-22 20:24:38 +0300

    22 okt

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 app.use(function(request, response, next) {
-
+    let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
     let now = new Date();
-    let hour = now.getHours();
+    let day = now.getDay();
+    let date = now.getDate()
+    let hour = now.getHours() + "";
+    if (hour.length == 1) {
+        hour = 0 + hour;
+    }
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
-    let data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
-    console.log(data);
+    let data = `дата: ${date} ${days[day]} ${hour}:${minutes}:${seconds}`;
     fs.appendFile("server.log", data + "\n", function() {});
     next();
 });
 
 app.get("/", function(request, response) {
-     app.use(express.static(__dirname)); 
+    app.use(express.static(__dirname));
     response.sendFile(__dirname + "/index.html");
 });
 app.get("/ALEO/aleo.html", function(request, response) {
@@ -24,7 +28,7 @@ app.get("/ALEO/aleo.html", function(request, response) {
 app.get("/modus-versus/modusversus.html", function(request, response) {
     app.use(express.static(__dirname + "/modus-versus"));
     response.sendFile(__dirname + "/modus-versus/modusversus.html");
-}); 
+});
 app.get("/CleanMag/cleanmag.html", function(request, response) {
     app.use(express.static(__dirname + "/CleanMag"));
     response.sendFile(__dirname + "/CleanMag/cleanmag.html");
